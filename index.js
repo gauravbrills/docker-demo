bc68947d799ed30ee59ab2552ba212e9e37c4db6
Gaurav Rawat <gauravbrills14@gmail.com>
2019-02-06 01:10:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello from Terraform and Docker!');
+  res.send('Hola from Terraform and Docker!');
 });
 
 var server = app.listen(3000, function () {
